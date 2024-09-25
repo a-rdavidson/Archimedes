bef4db585a75d396cd8c4900600297667ae45fac
Andrew <adavidson2022@gmail.com>
2024-09-25 19:32:56 +0100

    Added basic HTML page to replace client code

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -32,27 +32,61 @@ using rgb_matrix::FrameCanvas;
 using rgb_matrix::RGBMatrix;
 using rgb_matrix::StreamReader;
 
+std::string read_file(const std::string& file_path) {
+  std::ifstream file(file_path); 
+  if (! file.is_open()) {
+    return ""; // empty return if file cannot be openned
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();     // read whole file into buffer
+  return buffer.str();
+}
+
+
 int main() {
   crow::SimpleApp app; 
 
+  /* 
+   * Route to serve HTML page
+   */ 
+  
+  CROW_ROUTE(app, "/")
+  ([]() {
+    std::string html_content = read_file("../html/upload.html");
+    if(html_content.empty()) {
+      return crow::response(404, "HTML file not found");
+    }
+    return crow::response(html_content);
+
+  });
+  
+ /* 
+   * Route to handle favicon.ico request
+   */ 
+
+  CROW_ROUTE(app, "/favicon.ico")
+  ([]() {
+    return crow::response(200, "");  // Respond with an empty body to prevent the 404 error
+  });
+  
+  /* 
+   * Route to handle file & flags upload
+   */ 
+  
   CROW_ROUTE(app, "/upload").methods(crow::HTTPMethod::Post)
   ([](const crow::request& req) {
-    std::string file_data = req.body; 
-    std::string content_type = req.get_header_value("Content-Type"); 
-    std::cout << content_type << std::endl;
-/*    if(!is_valid_mime_type(content_type)) {
-      
-      return crow::response(400, "Invalid File Type. Only JPG, PNG, GIF, & MP4"); 
-    }
-*/
+    
+    std::string file_data = req.body;
     std::string file_path = "/tmp/uploaded_file"; 
+    
     std::ofstream file(file_path, std::ios::binary); 
     file.write(req.body.c_str(), req.body.size()); 
     file.close();
 
     if(!is_valid_file_signature(file_path)) {
       try {
-        std::filesystem::remove(file_path); 
+        //std::filesystem::remove(file_path); 
       } catch (const std::filesystem::filesystem_error& err) {
         std::cout << "Filesystem error: " << err.what() << std::endl; 
       }
@@ -73,10 +107,10 @@ int main() {
     }
     
     int num_flags = delim_count + 1;
-    char ** argv; string_to_char_array(flags, num_flags);
+    char ** client_argv; string_to_char_array(flags, num_flags);
     
     for (int i = 0; i < num_flags; i++) {
-      std:: cout << argv[i] <<  std::endl;
+      std:: cout << client_argv[i] <<  std::endl;
     }
 
     RGBMatrix::Options matrix_options; 
@@ -85,10 +119,11 @@ int main() {
     runtime_opt.drop_priv_user = getenv("SUDO_UID"); 
     runtime_opt.drop_priv_group = getenv("SUDO_GID"); 
 
-    free_char_array(argv, num_flags); 
+    free_char_array(client_argv, num_flags); 
 
     return crow::response(200, "Image received and processed");
   });
+
   app.port(8080).multithreaded().run(); 
   return 0; 
 }
